68c8de703e6af98a7b94a1aec0a170c8cd36eb15
MarquisPeroni <marco_p2009@live.it>
2024-05-14 16:22:57 +0200

    secondcommit

--- index.php
+++ index.php
@@ -3,18 +3,41 @@
 include 'Libro.php';
 include 'DVD.php';
 
-$libro1 = new Libro('Il Signore degli Anelli', 'J.R.R. Tolkien', 1954);
-$libro2 = new Libro('1984', 'George Orwell', 1949);
-$dvd1 = new DVD('Inception', 'Christopher Nolan', 2010);
+$libri = [
+    new Libro('Il Signore degli Anelli', 'J.R.R. Tolkien', 1954),
+    new Libro('1984', 'George Orwell', 1949),
+    new Libro('Cime tempestose', 'Emily Brontë', 1847),
+    new Libro('Orgoglio e pregiudizio', 'Jane Austen', 1813),
+    new Libro('Il conte di Montecristo', 'Alexandre Dumas', 1844),
+    new Libro('Moby Dick', 'Herman Melville', 1851),
+    new Libro('Guerra e pace', 'Lev Tolstoj', 1869),
+    new Libro('Il giovane Holden', 'J.D. Salinger', 1951),
+    new Libro('1984', 'George Orwell', 1949),
+    new Libro('Anna Karenina', 'Lev Tolstoj', 1877),
+    new Libro('Il processo', 'Franz Kafka', 1925),
+    new Libro('Don Chisciotte della Mancia', 'Miguel de Cervantes', 1605),
+];
+
+$film = [
+    new DVD('Inception', 'Christopher Nolan', 2010),
+    new DVD('Interstellar', 'Christopher Nolan', 2014),
+    new DVD('Pulp Fiction', 'Quentin Tarantino', 1994),
+    new DVD('Il padrino', 'Francis Ford Coppola', 1972),
+    new DVD('La vita è bella', 'Roberto Benigni', 1997),
+];
 
 echo "Numero totale di libri nella biblioteca: " . Libro::contaLibri() . "\n";
 echo "Numero totale di DVD nella biblioteca: " . DVD::contaDVD() . "\n";
 
-// Restituzione del libro prestato
-$libro1->restituisci();
-echo "Numero totale di libri nella biblioteca dopo la restituzione: " . Libro::contaLibri() . "\n";
+// Presta un libro
+$libro_da_prestare = $libri[5]; 
+$libro_da_prestare->presta();
+
+echo "Numero totale di libri nella biblioteca dopo il prestito: " . Libro::contaLibri() . "\n";
 
 ?>
+
+
 
 
 
